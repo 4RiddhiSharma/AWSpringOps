9d2eccf84e2ab0201d113445d825020d898e276e
4RiddhiSharma <136804969+4RiddhiSharma@users.noreply.github.com>
2025-03-07 00:01:37 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-docker pull 13riddhi/myspringapp
+docker pull 13riddhi/myspringapp:v1
 
-docker run -d -p 8080:8080 13riddhi/myspringapp
+docker run -d -p 8080:8080 13riddhi/myspringapp:v1
 
 
